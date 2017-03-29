63400b7adc1ea08fb05eee0656d07436eabd9cdc
tga_cottacush <taiwo@cottacush.com>
2017-03-29 15:41:10 +0100

    Format code

--- src/OauthClient/Oauth2Client.php
+++ src/OauthClient/Oauth2Client.php
@@ -116,7 +116,8 @@ class Oauth2Client
     public function fetchAccessTokenWithClientCredentials()
     {
         $this->validateTokenParams();
-        try {
+        try
+        {
             $response = $this->curl->setOption(
                 CURLOPT_POSTFIELDS,
                 http_build_query(array(
